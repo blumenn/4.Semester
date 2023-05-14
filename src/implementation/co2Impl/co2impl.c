68da07cb785c70d76c31c8a0b29577e22ae7a8d9
blumenn <anders.blumensaat@gmail.com>
2023-05-14 21:47:56 +0200

    again..

--- src/implementation/co2Impl/co2impl.c
+++ src/implementation/co2Impl/co2impl.c
@@ -1,7 +1,7 @@
 #include <stddef.h>
 #include "co2.h"
-#include "/drivers/mh_z19.h"
-#include "../../../drivers/serial.h"
+#include "../../drivers/mh_z19.h"
+#include "../../drivers/serial.h"
 #include <stdio.h>
 uint16_t ppm;
 
