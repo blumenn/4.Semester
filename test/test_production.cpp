1bb38dff2cf4ce43288bca8d091a4e0de8eba7de
sajit <sajithijan1245@gmail.com>
2023-05-18 10:35:05 +0200

    humidity updated

--- test/test_production.cpp
+++ test/test_production.cpp
@@ -10,6 +10,7 @@ extern "C"
 #include "../src/implementation/co2Impl/mh_z19.h"
 #include "../src/implementation/co2Impl/co2.h" 
 #include "../src/implementation/tempImpl/temperaturImpl.h"
+#include "../src/implementation/humidityImpl/humidityImpl.h"
 #include "../drivers/hih8120.h"
 }
 
@@ -97,6 +98,47 @@ TEST_F(TempImplTest, TestTempImplInit) {
 TEST_F(TempImplTest, TestTempImplMeasure) {
     hih8120_measure_fake.return_val = HIH8120_OK;
     tempimpl_measure();
+    
+    EXPECT_EQ(hih8120_measure_fake, 1);
+} 
+
+
+
+
+
+
+
+
+
+
+DEFINE_FFF_GLOBALS	
+
+// Define fake functions
+//FAKE_VALUE_FUNC(hih8120_driverReturnCode_t, hih8120_initialise);
+FAKE_VALUE_FUNC(hih8120_driverReturnCodes, hih8120_measure);
+FAKE_VALUE_FUNC(int16_t, hih8120_getHumidityPercent_x10);
+
+class HumImplTest : public testing::Test {
+protected:
+    void SetUp() override {
+        // Resets the fake functions
+        RESET_FAKE(hih8120_measure);
+        RESET_FAKE(hih8120_getHumidityPercent_x10);
+    }
+    void TearDown() override {
+
+	}
+};
+
+TEST_F (HumImplTest, TestGetMeasurement) {
+	int Hum;
+	Hum = humimpl_getMeasurement();
+	EXPECT_EQ(0, Hum);
+}
+
+TEST_F(HumImplTest, TestHumImplMeasure) {
+    hih8120_measure_fake.return_val = HIH8120_OK;
+    humimpl_measure();
     
     EXPECT_EQ(hih8120_measure_fake, 1);
 } 
