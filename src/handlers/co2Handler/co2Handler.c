b4e416dff8424b044c18587101425023cff65b17
blumenn <anders.blumensaat@gmail.com>
2023-05-19 22:18:21 +0200

    Corrections from building in microship studio.

--- src/handlers/co2Handler/co2Handler.c
+++ src/handlers/co2Handler/co2Handler.c
@@ -1,25 +1,56 @@
 #include <stdint.h>
+#include "ATMEGA_FreeRTOS.h"
+#include "task.h"
+#include "co2Handler.h"
+#include "SensorData.h"
+#include "../../implementation/co2Impl/co2.h"
+#include "mh_z19.h"
+#include "task.h"
 
-void create_co2handler_task(){
-    xTaskCreate(
-    co2_handler_task, 
-    "co2_handler_task",
-    1000,
-    NULL,
-    1,
-    NULL);
+// Forward declaration of co2_handler_task
+void co2_handler_task(void *pvParameters);
 
+TaskHandle_t co2HandlerTaskHandle = NULL;
+
+void create_co2handler_task()
+{
+	BaseType_t taskCreated;
+	taskCreated = xTaskCreate(
+	co2_handler_task,
+	"co2_handler_task",
+	1000,
+	NULL,
+	1,
+	&co2HandlerTaskHandle
+	);
 }
 
-void co2_init(){
-    co2impl_init();
+void run_co2handler_task()
+{
+	SensorData data;
+	data.sensorName = "Co2Sensor";
+	mh_z19_returnCode_t returnCode = co2impl_measure();
+	data.status = (returnCode == MHZ19_OK) ? SENSOR_STATUS_OK : SENSOR_STATUS_ERROR;
+	data.data = co2impl_getMeasurement();
+	xQueueSend(sensorQueue, &data, portMAX_DELAY);
 }
 
-
-uint16_t co2_getMeasurement(){
-co2impl_measure();
-co2impl_getMeasurement();
+void co2_handler_task(void *pvParameters)
+{
+	for(;;)
+	{
+		run_co2handler_task();
+		vTaskDelay(pdMS_TO_TICKS(10000));
+	}
 }
 
+void co2_init()
+{
+	co2impl_init();
+	create_co2handler_task();
+}
 
-// uint16_t co2_getAvg();
+uint16_t co2_getMeasurement()
+{
+	return co2impl_getMeasurement();
+}
