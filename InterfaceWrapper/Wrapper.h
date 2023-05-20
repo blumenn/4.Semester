f3e597198dc139339c6490e5e4dcc4c0c196dba8
sajit <sajithijan1245@gmail.com>
2023-05-20 18:04:48 +0200

    Update queue

--- InterfaceWrapper/Wrapper.h
+++ InterfaceWrapper/Wrapper.h
@@ -1,8 +1,14 @@
 #include <stdint.h>
-#include <stdint.h>
 #include <lora_driver.h>
 #include <ATMEGA_FreeRTOS.h>
 #include <semphr.h>
 #include <status_leds.h>
+#include <queue.h>
 
 lora_driver_payload_t wrapperhandler();
+
+typedef struct {
+    char *sensorName;
+    SensorStatus status;
+    uint16_t data;
+} SensorData;
