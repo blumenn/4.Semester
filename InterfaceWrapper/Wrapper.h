6074e660cc87a80c9410b61c3bf9e54ff7f5a7de
pilsner91 <morten_oel@hotmail.com>
2023-05-22 10:40:28 +0200

    made some minor changes

--- InterfaceWrapper/Wrapper.h
+++ InterfaceWrapper/Wrapper.h
@@ -1,8 +1,9 @@
 #include <stdint.h>
-#include <stdint.h>
 #include <lora_driver.h>
 #include <ATMEGA_FreeRTOS.h>
 #include <semphr.h>
 #include <status_leds.h>
+#include "../src/SensorData.h"
 
 lora_driver_payload_t wrapperhandler();
+
