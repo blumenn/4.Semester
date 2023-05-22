887a6488ced1324c55b0445e91744f43f8789960
pilsner91 <morten_oel@hotmail.com>
2023-05-22 10:30:49 +0200

    made it so that it will save the latestData

--- Wrapper.c
+++ Wrapper.c
@@ -8,11 +8,22 @@
 #include <semphr.h>
 #include <status_leds.h>
 
-static lora_driver_payload_t _uplink_payload;
 
+static lora_driver_payload_t _uplink_payload;
+QueueHandle_t xQueue;
+static latestData; 
 void wrapper_init(){
 	_uplink_payload.len = 6;
 	_uplink_payload.portNo = 2;
+	xQueue = xQueueCreate(15,sizeof(SensorData *));
+	if (xQueue == NULL)
+	{
+		// return fejl
+		return
+	}
+
+	
+	
 }
 
  lora_driver_payload_t wrapperhandler()
@@ -27,6 +38,45 @@ uint16_t hum = hum_getMeasurement();
 	_uplink_payload.bytes[3] = temp & 0xFF;
 	_uplink_payload.bytes[4] = co2_ppm >> 8;
 	_uplink_payload.bytes[5] = co2_ppm & 0xFF;
-return _uplink_payload;
-	
+return _uplink_payload;	
 }
+wrapper_task(){
+	SensorData data;
+	if( xQueue != NULL ) //checks if queue is made
+   {
+     while (xQueueIsQueueEmptyFromISR(xQueue)== pdFAIL)
+	 {
+		if( xQueueReceive( xQueue,
+                         &( data ),
+                         ( TickType_t ) 10 ) == pdPASS )
+      {
+
+         saveData(data);
+		 
+      }
+	 }
+	 
+      
+   }
+}
+
+
+void saveData(SensorData data){
+	if(data.status==SENSOR_STATUS_OK){
+	if (data.sensorName=="Co2Sensor")
+	{
+	latestData.CO2 = data;
+	return;
+	}
+	if (data.sensorName == "Humidity")
+	{
+		latestData.hum = data;
+	}
+	if (data.sensorName == "Temperature")
+	{
+		latestData.temp = data;
+	}
+
+}
+}
+
