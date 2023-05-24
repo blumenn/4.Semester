ad794a6d90b3d4d671023a21390b52e31092e9db
Anders Blumensaat <95355631+blumenn@users.noreply.github.com>
2023-05-24 13:34:53 +0200

    Update co2.h

--- src/implementation/co2Impl/co2.h
+++ src/implementation/co2Impl/co2.h
@@ -1,6 +1,5 @@
 #include <stdint.h>
-// #include "mh_z19.h"
-#include"../../../drivers/mh_z19.h"
+#include "src/drivers/mh_z19.h"
 void co2impl_init();
 mh_z19_returnCode_t co2impl_measure();
 // uint16_t co2impl_getAvg();
