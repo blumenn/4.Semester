2ded1a8dd1f8dfb56f83ccd9bdaa5efaa78611ed
blumenn <anders.blumensaat@gmail.com>
2023-05-31 16:05:59 +0200

    fix#6

--- src/handlers/servoHandler/servoHandler.c
+++ src/handlers/servoHandler/servoHandler.c
@@ -1,5 +1,5 @@
 #include "servoHandler.h"
-#include "dataCollection.h"
+#include "../../DataCollection/dataCollection.h"
 #include "../../implementation/servo/servoImpl.h"
 #include <ATMEGA_FreeRTOS.h>
 #include <semphr.h>
