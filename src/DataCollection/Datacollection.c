77da9468d90e0119067410c233b9bd7e025e3222
blumenn <anders.blumensaat@gmail.com>
2023-05-31 15:56:58 +0200

    fix#3

--- src/DataCollection/Datacollection.c
+++ src/DataCollection/Datacollection.c
@@ -1,7 +1,7 @@
 #include "../src/handlers/co2Handler/co2Handler.h"
 #include "../src/handlers/HumidityHandler/humidityHandler.h"
 #include "../src/handlers/temperturHandler/temperturHandler.h"
-#include "../DataCollection/datacollection.h"
+#include "datacollection.h"
 #include <ATMEGA_FreeRTOS.h>
 #include <semphr.h>
 #include <status_leds.h>
